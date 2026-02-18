b19a177543fdac5f49bb4c6da57e969b31e1b8af
dodo <108921606+doyeon0307@users.noreply.github.com>
2026-02-18 23:14:11 +0900

    [level 2] Title: 큰 수 만들기, Time: 97.09 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -2,32 +2,17 @@ def solution(number, k):
     answer = ''
     
     stack = []
-    max_stack = len(number) - k
     
     for n in number:
-        if k <= 0:
-            stack.append(n)
-            continue
-        
-        if not stack:
-            stack.append(n)
-            continue
-        
-        while stack:
-            prev = stack.pop()
-            if n > prev:
-                k -= 1
-                if k <= 0:
-                    break
-                continue
-            else:
-                stack.append(prev)
-                break
-                
+        while stack and k > 0 and n > stack[-1]:
+            stack.pop()
+            k -= 1 
         stack.append(n)
 
-                
-    for s in stack[:max_stack]:
+    if k > 0:
+        stack = stack[:-k]
+        
+    for s in stack:
         answer += s
     
     return answer
