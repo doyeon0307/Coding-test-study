c5a6046c3861a6fae7bf4dbcf5734bd557c5e6c8
dodo <108921606+doyeon0307@users.noreply.github.com>
2026-03-13 11:30:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.08 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,33 @@
+def solution(n, wires):
+    answer = 100000000
+    
+    con = [[] for _ in range(n + 1)]
+    for w in wires:
+        a, b = map(int, w)
+        con[a].append(b)
+        con[b].append(a)
+
+    for w in wires:
+        a, b = map(int, w)
+        con[a].remove(b)
+        con[b].remove(a)
+
+        stack = []
+        tree = set()
+        
+        stack.append(a)
+        tree.add(a)
+           
+        while stack:
+            l = stack.pop()
+            for i in con[l]:
+                if not (i in tree):
+                    stack.append(i)
+                    tree.add(i)
+        
+        con[a].append(b)
+        con[b].append(a)
+
+        answer = min(answer, abs(n - len(tree) * 2))
+        
+    return answer
