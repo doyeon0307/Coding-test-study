58a22e3de3e36deb8ba1efda7cb7d7d7b29a1a98
dodo <108921606+doyeon0307@users.noreply.github.com>
2026-05-12 12:51:13 +0900

    [level 2] Title: 가장 큰 수, Time: 1165.63 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,26 +1,12 @@
-import math
-
 def extendX(x, max_l):
-    return str(x * math.ceil(max_l / len(x)))[:12]
+    return str(x * math.ceil(max_l / len(x)))
         
 
-def solution(numbers):
-    answer = []
+def solution(numbers):  
+    numbers.sort(key = lambda x : str(x) * 12, reverse = True)
     
-    max_l = len(str(numbers[0]))
+    answer = ''
     for n in numbers:
-        answer.append(str(n))
-        if len(str(n)) > max_l:
-            max_l = len(str(n))
+        answer += str(n)
     
-    answer.sort(key = lambda x : str(x) * 3, reverse = True)
-    
-    result = ''
-    for a in answer:
-        result += a
-    
-    f = 0
-    while result[f] == '0' and f < len(result) - 1:
-        f += 1
-    
-    return result[f:]
+    return str(int(answer))
